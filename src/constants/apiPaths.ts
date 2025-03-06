cc2832f22baa74a0ddf8bdfdd3490361a6c391f1
Yan Litvinenko <glavnyydirizhor@gmail.com>
2025-03-06 21:47:48 +0300

    change path import

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://lhhsz36dei.execute-api.us-west-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://citfu68rd9.execute-api.us-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
